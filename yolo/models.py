4fefe7f3d8e933e850e6ddd3c7ffca964744a9bf
Matthew Dutson <mattdutson@icloud.com>
2021-06-25 14:48:31 -0500

    Add post-backbone convolution layers

--- yolo/models.py
+++ yolo/models.py
@@ -2,6 +2,8 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.layers import *
 
+LEAK_ALPHA = 0.1
+
 
 # Thanks to github.com/zzh8829 for information on how to read
 # Darknet weight files. I use a few lines of their weight-loading code
@@ -9,6 +11,8 @@ from tensorflow.keras.layers import *
 # https://github.com/zzh8829/yolov3-tf2/blob/master/yolov3_tf2/utils.py
 
 
+# Original architecture specification:
+# https://github.com/pjreddie/darknet/blob/master/cfg/yolov3.cfg
 def yolo_v3(darknet_weights=None):
     inputs = Input(shape=(None, None, 3))
     x = inputs
@@ -19,44 +23,52 @@ def yolo_v3(darknet_weights=None):
     else:
         w = None
 
+    # Darknet backbone
     x = _conv_block(x, w, 32)
-
     x = _conv_block(x, w, 64, strides=2)
     for _ in range(1):
         x = _residual_block(x, w, 64)
-
     x = _conv_block(x, w, 128, strides=2)
     for _ in range(2):
         x = _residual_block(x, w, 128)
-
     x = _conv_block(x, w, 256, strides=2)
     for _ in range(8):
         x = _residual_block(x, w, 256)
-
     x = _conv_block(x, w, 512, strides=2)
     for _ in range(8):
         x = _residual_block(x, w, 512)
-
     x = _conv_block(x, w, 1024, strides=2)
     for _ in range(4):
         x = _residual_block(x, w, 1024)
 
+    for _ in range(3):
+        x = _conv_block(x, w, 512, kernel_size=1)
+        x = _conv_block(x, w, 1024)
+    x = _conv_block(x, w, 255, kernel_size=1, batch_norm=False)
+
     return tf.keras.Model(inputs=inputs, outputs=x)
 
 
-def _conv_block(x, w, filters, kernel_size=3, strides=1):
-    conv = Conv2D(filters, kernel_size, strides=strides, padding='same', use_bias=False)
+def _conv_block(x, w, filters, kernel_size=3, strides=1, batch_norm=True):
+    conv = Conv2D(filters, kernel_size, strides=strides, padding='same', use_bias=not batch_norm)
     x = conv(x)
-    bn = BatchNormalization()
-    x = bn(x)
-    x = LeakyReLU(alpha=0.1)(x)
+    if batch_norm:
+        bn = BatchNormalization()
+        x = bn(x)
+    else:
+        bn = None
+    x = LeakyReLU(alpha=LEAK_ALPHA)(x)
 
     if w is not None:
-        # TF stores BN weights as [gamma, beta, mean, variance]. Darknet
-        # stores them as [beta, gamma, mean, variance].
-        w_bn = np.fromfile(w, dtype=np.float32, count=4 * filters)
-        w_bn = w_bn.reshape((4, filters))[[1, 0, 2, 3]]
-        bn.set_weights(w_bn)
+        if batch_norm:
+            # TF stores BN weights as [gamma, beta, mean, variance].
+            # Darknet stores them as [beta, gamma, mean, variance].
+            w_bn = np.fromfile(w, dtype=np.float32, count=4 * filters)
+            w_bn = w_bn.reshape((4, filters))[[1, 0, 2, 3]]
+            bn.set_weights(w_bn)
+        else:
+            b_conv = np.fromfile(w, dtype=np.float32, count=filters)
+            conv.bias.assign(b_conv)
 
         # A TF kernel has shape (h, w, c_in, c_out). A Darknet kernel
         # has shape (c_out, c_in, h, w).
@@ -64,7 +76,7 @@ def _conv_block(x, w, filters, kernel_size=3, strides=1):
         s_darknet = (s_tf[3], s_tf[2], s_tf[0], s_tf[1])
         w_conv = np.fromfile(w, dtype=np.float32, count=np.prod(s_darknet))
         w_conv = w_conv.reshape(s_darknet).transpose([2, 3, 1, 0])
-        conv.set_weights([w_conv])
+        conv.kernel.assign(w_conv)
 
     return x
 
